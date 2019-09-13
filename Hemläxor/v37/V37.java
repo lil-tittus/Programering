0e1d51930780c60c1b1712cb573f32eba63a79d9
lil-tittus <jared.tittus@elev.ga.ntig.se>
2019-09-13 12:58:56 +0200

    lagt in alla frågor

--- Hemläxor/v37/V37.java
+++ Hemläxor/v37/V37.java
@@ -4,17 +4,26 @@ public class V37 {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
-		System.out.println("Vad heter du");
+		System.out.println("Vad heter du?");
 		
 		String Förnamn = input.nextLine();
 		System.out.println(Förnamn);
 		
 	
-		System .out.println("Hur gammal är du?");
-		int gammal = input.nextInt();
+		System.out.println("Hur gammal är du?");
+		String gammal = input.nextLine();
 		
+		System.out.println("Vad är din address?");
+		String Adress = input.nextLine();
 		
+		System .out.println("Vad är ditt postnummer?");
+		String Postnummer = input.nextLine();
 		
+		System .out.println("Vilken stad bor du i?");
+		String Stad = input.nextLine();
+		
+		System .out.println("Vad är ditt telefonnummer?");
+		String Telefonnummer = input.nextLine();
 	}
 	
 }
