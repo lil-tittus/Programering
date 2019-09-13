b63d5505ec86cf4649594f9ff0b9ed54ba6ba83d
lil-tittus <jared.tittus@elev.ga.ntig.se>
2019-09-13 11:27:10 +0200

    Update V37.java
    Jag har lagt in en string nu.

--- Hemläxor/v37/V37.java
+++ Hemläxor/v37/V37.java
@@ -4,6 +4,12 @@ public class V37 {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
+		System.out.println("Vad heter du");
+		
+		String Förnamn = input.nextLine();
+		System.out.println(Förnamn);
+		
+		
 		
 	}
 	
